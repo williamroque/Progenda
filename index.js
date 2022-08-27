b7796cf42fe0a0370746aaa553f42f37c6a53ac7
William Roque <william.aroque@gmail.com>
2022-08-27 14:58:06 -0600

    Updated.

--- index.js
+++ index.js
@@ -153,11 +153,3 @@ const agenda = sortAgenda(parseAgenda(data));
 
 render(agenda);
 
-window.addEventListener('load', function(e) {
-    window.applicationCache.addEventListener('updateready', function(e) {
-        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-            window.applicationCache.swapCache();
-            window.location.reload();
-        }
-    }, false);
-}, false);
